741edcbb078113d5bb64cacca04a390c83b2707d
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2020-04-24 21:50:24 +0700

    Finish Read Student info from csv and create password

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include "Caro.h"
+
+int main()
+{
+	ifstream in("student.csv");
+	if (!in.is_open())
+	{
+		cout << "Cant open file" << endl;
+		return 1;
+	}
+	int nStudents = 2;
+	Student* aStudent = new Student[2];
+	importStudentsFromCSV(in, aStudent, nStudents);
+	createUserPassword(aStudent,nStudents);
+
+	return 0;
+}
